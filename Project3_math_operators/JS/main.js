dbaa3a78ad7d9c823ec6d9ac3dbf20c4dcfda3cf
saima-dot <8925300@gmail.com>
2025-12-02 17:43:58 -0500

    Add files via upload

---
+++ Project3_math_operators/JS/main.js
@@ -0,0 +1,74 @@
+// Function to perform addition and display result
+function showSum() {
+    let num1 = 5;
+    let num2 = 10;
+
+    // Calculate the sum
+    let sum = num1 + num2;
+
+    // Display the result in the paragraph with id "Math"
+    document.getElementById("Math").innerHTML = "The sum of " + num1 + " + " + num2 + " is: " + sum;
+}
+// Subtraction operation
+let number1 = 10;
+let number2 = 4;
+
+let result = number1 - number2;
+
+// Display the result in the console
+console.log("The result of the subtraction is:", result);
+// Multiplication operation
+let a = 6;
+let b = 7;
+let multiplication Result = a * b;
+console.log("Multiplication result:", multiplicationResult);
+
+// Division operation
+let x = 20;
+let y = 5;
+let division Result = x / y;
+console.log("Division result:", divisionResult);
+// Addition operation
+let num1 = 10;
+let num2 = 5;
+let additionResult = num1 + num2;
+console.log("Addition result:", additionResult);
+
+// Subtraction operation
+let num3 = 15;
+let num4 = 7;
+let subtractionResult = num3 - num4;
+console.log("Subtraction result:", subtractionResult);
+
+// Multiplication operation
+let num5 = 6;
+let num6 = 4;
+let multiplicationResult = num5 * num6;
+console.log("Multiplication result:", multiplicationResult);
+
+// Division operation
+let num7 = 20;
+let num8 = 4;
+let divisionResult = num7 / num8;
+console.log("Division result:", divisionResult);
+
+// Modulus operation (remainder of division)
+let num9 = 25;
+let num10 = 6;
+let modulusResult = num9 % num10;
+console.log("Modulus result (remainder):", modulusResult);
+
+// Exponentiation operation (power)
+let num11 = 2;
+let num12 = 3;
+let exponentiationResult = num11 ** num12;
+console.log("Exponentiation result (2^3):", exponentiationResult);
+// Increment operator
+let num 1 = 5;
+num1++;  // Increments num1 by 1
+console.log("After incrementing, num1 is:", num1);
+
+// Decrement operator
+let num 2 = 10;
+num2--;  // Decrements num2 by 1
+console.log("After decrementing, num2 is:", num2);
